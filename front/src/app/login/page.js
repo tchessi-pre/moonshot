6a98df9e261a26308d0650a553c7006ee98213f5
Tchessy <tchessy@agence-map.com>
2024-07-05 10:31:38 +0200

    Added shadcn config

--- front/src/app/login/page.js
+++ front/src/app/login/page.js
@@ -12,34 +12,36 @@ export default function Login() {
     e.preventDefault();
   };
 
-  return (   
-    <div className="flex min-h-screen items-center justify-center">
-      <form onSubmit={handleSubmit} className="w-full max-w-md">
-        <div>
-          <label htmlFor="email">Email</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Mot de passe</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <p>Pas encore inscrit ? <a href="/register">S'inscrire</a></p>
-        </div>
-        <button type="submit">Se Connecter</button>
-      </form>
-    </div>
-  );
+  return (
+		<div className='flex items-center justify-center min-h-screen'>
+			<form onSubmit={handleSubmit} className='w-full max-w-md'>
+				<div>
+					<label htmlFor='email'>Email</label>
+					<input
+						type='email'
+						id='email'
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+						required
+					/>
+				</div>
+				<div>
+					<label htmlFor='password'>Mot de passe</label>
+					<input
+						type='password'
+						id='password'
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+						required
+					/>
+				</div>
+				<div>
+					<p>
+						Pas encore inscrit ? <a href='/register'>S'inscrire</a>
+					</p>
+				</div>
+				<button type='submit'>Se Connecter</button>
+			</form>
+		</div>
+	);
 }
