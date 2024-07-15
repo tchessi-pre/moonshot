f73c8cdb819d529f787703f6480dfaaf8bba69b4
morgane-marechal <morgane.marechal@laplateforme.io>
2024-07-15 14:12:49 +0200

    last change

--- front/src/app/informations_private/page.js
+++ front/src/app/informations_private/page.js
@@ -8,39 +8,41 @@ import { Badge } from "@/components/ui/badge"
 export default function Login() {
 	return (
 		<main
-			className='relative flex flex-col items-center justify-center h-fit bg-center bg-cover'
+			className='relative flex flex-col items-center justify-center min-h-screen bg-center '
 			style={{ backgroundImage: "url('/assets/bground.jpg')" }}
 		>
-            <div>
-                <span>
-                    John Doe
-                </span>
-            </div>
-            <div>
-                <Button type='submit' className='w-full mt-6'>
-					Modifier informations générales
-				</Button>
-            </div>
-            <div>
-                <span>
-                    Parle: Anglais, Italien, Français
-                </span>
-            </div>
-            <div>
-                <span>
-                    Marseille, France
-                </span>
-            </div>
+            <div className="bg-cover bg-white bg-opacity-50 p-10 rounded-lg m-10">
+                <div>
+                    <span>
+                        John Doe
+                    </span>
+                </div>
+                <div>
+                    <Button type='submit' className='w-full mt-6'>
+                        Modifier informations générales
+                    </Button>
+                </div>
+                <div>
+                    <span>
+                        Parle: Anglais, Italien, Français
+                    </span>
+                </div>
+                <div>
+                    <span>
+                    Localisation: Marseille, France
+                    </span>
+                </div>
 
-			<div className='flex items-center justify-center min-h-screen mx-2'>
-				<Form/>
-			</div>
-            <div className='flex items-center justify-center min-h-screen mx-2'>
-                <span>Langues parlées :</span>
-                <Badge variant="secondary">Secondary</Badge> 
-                <Badge variant="secondary">Secondary</Badge>
-                <Badge variant="secondary">Secondary</Badge>
-			</div>
+                <div className='flex items-center justify-center mx-2'>
+                    <Form/>
+                </div>
+                <div className='flex items-center justify-center  mx-2'>
+                    <span>Langues parlées :</span>
+                    <Badge variant="secondary">Anglais</Badge> 
+                    <Badge variant="secondary">Italien</Badge>
+                    <Badge variant="secondary">Français</Badge>
+                </div>
+            </div>
 		</main>
 	);
 }
