fc2bda5bfcbab1ce34b79aa0a51c15beea95485c
Tchessy <tchessy@agence-map.com>
2024-07-15 14:43:51 +0200

    Added cuisine page merge

--- front/src/components/EventCard.js
+++ front/src/components/EventCard.js
@@ -2,24 +2,24 @@ import React from 'react';
 
 const EventCard = ({ image, title, subtitle, description, buttonText }) => (
 	<div
-		className='flex overflow-hidden bg-white shadow-md transform transition-transform duration-300 hover:scale-105'
-		style={{ height: '200px', width: '480px' }}
+		className='flex overflow-hidden transition-transform duration-300 transform bg-white shadow-md hover:scale-105'
+		style={{ height: '200px', width: '430px', marginRight: '20px' }}
 	>
 		<div className='w-1/3 h-full'>
 			<img src={image} alt={title} className='object-cover w-full h-full' />
 		</div>
 		<div className='flex flex-col justify-between w-1/2 p-6'>
 			<div>
-				<div className='inline-block w-full p-1 text-white bg-gray-500 uppercase text-sm transition-colors duration-300 hover:bg-gray-700'>
+				<div className='inline-block w-full p-1 text-sm text-white uppercase transition-colors duration-300 bg-gray-500 hover:bg-gray-700'>
 					{subtitle}
 				</div>
-				<h3 className='text-xl font-bold mt-3 uppercase transition-colors duration-300 hover:text-gray-700'>
+				<h3 className='mt-3 text-xl font-bold uppercase transition-colors duration-300 hover:text-gray-700'>
 					{title}
 				</h3>
 				<p className='mb-4 text-sm text-orange-600'>{description}</p>
 			</div>
 			<div>
-				<button className='text-blue-500 hover:underline transition-transform duration-300 hover:scale-110'>
+				<button className='text-blue-500 transition-transform duration-300 hover:underline hover:scale-110'>
 					{buttonText}
 				</button>
 			</div>
