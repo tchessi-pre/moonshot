9fc5525cdbe7e6fc5f8625b4ae37b49b2bb87f7a
Tchessy <tchessy@agence-map.com>
2024-07-16 12:12:21 +0200

    Added vertical menu url

--- front/src/app/musique/page.js
+++ front/src/app/musique/page.js
@@ -27,17 +27,17 @@ const musicEvents = [
 
 const MusicPage = () => {
     return (
-        <div className="bg-cover bg-center min-h-screen" style={{ backgroundImage: 'url("/assets/bground.jpg")' }}>
-            <div className="container mx-auto py-8 px-4">
-                <h1 className="text-3xl md:text-5xl lg:text-7xl font-bold text-white text-center mb-8 font-roboto-condensed uppercase">ÉCOUTEZ LE MONDE CHANTER</h1>
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="min-h-screen bg-center bg-cover" style={{ backgroundImage: 'url("/assets/bground.jpg")' }}>
+            <div className="container px-4 py-8 mx-auto">
+                <h1 className="mb-8 text-3xl font-bold text-center text-white uppercase md:text-5xl lg:text-7xl font-roboto-condensed">ÉCOUTEZ LE MONDE CHANTER</h1>
+                <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
                     {musicEvents.map((event, index) => (
-                        <div key={index} className="relative group overflow-hidden rounded-lg shadow-md">
-                            <img src={event.image} alt={event.title} className="w-full h-64 md:h-48 lg:h-64 object-cover" />
-                            <div className="absolute bottom-0 left-0 right-0 bg-white bg-opacity-80 p-4 transform translate-y-full group-hover:translate-y-0 transition duration-300 ease-in-out">
-                                <h2 className="text-xl md:text-2xl font-bold text-black">{event.title}</h2>
-                                <p className="text-sm md:text-lg text-orange-600">{event.subtitle}</p>
-                                <button className="text-blue-500 hover:underline mt-4">{event.buttonText}</button>
+                        <div key={index} className="relative overflow-hidden rounded-lg shadow-md group">
+                            <img src={event.image} alt={event.title} className="object-cover w-full h-64 md:h-48 lg:h-64" />
+                            <div className="absolute bottom-0 left-0 right-0 p-4 transition duration-300 ease-in-out transform translate-y-full bg-white bg-opacity-80 group-hover:translate-y-0">
+                                <h2 className="text-xl font-bold text-black md:text-2xl">{event.title}</h2>
+                                <p className="text-sm text-orange-600 md:text-lg">{event.subtitle}</p>
+                                <button className="mt-4 text-blue-500 hover:underline">{event.buttonText}</button>
                             </div>
                         </div>
                     ))}
