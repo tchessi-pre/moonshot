0e56d562b2da24e449b54aced8b734fb533f48e2
morgane-marechal <morgane.marechal@laplateforme.io>
2024-07-17 15:08:15 +0200

    commit before pull

--- front/src/app/login/Form.js
+++ front/src/app/login/Form.js
@@ -20,7 +20,7 @@ const Form = () => {
 		console.log('Password:', password);
 
 		try {
-			const response = await fetch('https://127.0.0.1:8000/api/login', {
+			const response = await fetch('http://127.0.0.1:8000/api/login', {
 				method: 'POST',
 				headers: {
 					'Content-Type': 'application/json',
